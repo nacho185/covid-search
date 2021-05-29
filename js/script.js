e1e257e39183807eca78cd6ab475d7ac44d10282
Ignacio Aguirre <ignacio13542@gmail.com>
2021-05-28 22:14:44 -0500

    🔨🔨fixed searchbar, andadded js
    fixed search bar to look more modern, and added javascript to click button when you hit enter key

--- js/script.js
+++ js/script.js
@@ -84,3 +84,16 @@ fetch(firstUrl + china + secondUrl)
   .catch((err) => {
     console.log(err);
   });
+/*enter === search*/
+
+// Execute a function when the user releases a key on the keyboard
+inputValue.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    searchButton.click();
+  }
+});
+/*end enter === search*/
